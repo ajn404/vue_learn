03eec20de10d4bfdfaafd5d2ebd7971ed7bd9934
ajn404 <huiyuening00@gmail.com>
2021-04-14 20:05:54 +0800

    hello vue

---
+++ helloV/src/components/HelloVue.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="hello">
+    vue生命周期
+   <br/>
+   <img src="https://ask.qcloudimg.com/http-save/devdocs/2nenjp4bnk.png" height="1000px">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- 使用scoped属性保证该CSS样式仅用于本组件 -->
+<style scoped>
+.hello{
+  top:0;
+  bottom:0;
+  position:fixed;
+  overflow-y:scroll;
+  background-color:#123456;
+  color:white
+}
+</style>
